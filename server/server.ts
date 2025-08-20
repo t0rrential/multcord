818c0384ab7c1dd564a3567f8e41cd260b04a5d3
bryan <hargoescorgo5@gmail.com>
2025-08-20 17:58:48 -0500

    feat: real basic api routes

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import { Client } from 'discord.js-selfbot-v13';
+import { Client, Collection, Guild, OAuth2Guild } from 'discord.js-selfbot-v13';
 
 dotenv.config({path: '../.env'});
 const app = express();
@@ -8,9 +8,42 @@ let token: string = process.env.DISCORD_TOKEN; // prod use only
 const port = 3000;
 const client = new Client();
 
+let servers: Collection<string, OAuth2Guild> = new Collection();
+
+async function updateServers() {
+    servers = await client.guilds.fetch();
+}
+
+client.on('guildCreate', async (guild) => {
+    updateServers();
+});
+
 client.on('ready', async () => {
     console.log(`${client.user?.username} is ready!`);
+
+    app.get('/api/discord/servers', async (req, res) => {
+        res.json(servers.map(async server => {
+            const fullServer = await server.fetch();
+
+            return {
+                id: fullServer.id,
+                members: fullServer.memberCount,
+                name: fullServer.name,
+            }
+        }));
+    });
+
+    app.get('/api/discord/servers/:channelId', async (req, res) => {
+        const server = await servers.filter(server => server.id === req.params.channelId);
+        const fullServer = await server[0].fetch(); // fetch server, i hate filter
+
+        res.json(fullServer.channels.map(channel => {
+            return {
+                id: channel.id,
+                name: channel.name,
+            }
+        }));
+    });
 })
 
-console.log(process.env.DISCORD_TOKEN);
 client.login(token);
